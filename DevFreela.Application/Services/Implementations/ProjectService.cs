462c0d08683dfb3894f04bc51a64e6a7d34ab767
Samuel Carvalho <samuelcarvalho2000@outlook.com>
2025-08-18 22:51:59 -0300

    feat: implementando as operações e salvando no banco de dados

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -20,6 +20,7 @@ namespace DevFreela.Application.Services.Implementations
             var project = new Project(inputModel.Title, inputModel.Description, inputModel.IdClient, inputModel.IdFreelancer, inputModel.TotalCost);
 
             _dbContext.Projects.Add(project);
+            _dbContext.SaveChanges();
 
             return project.Id;
         }
@@ -28,7 +29,9 @@ namespace DevFreela.Application.Services.Implementations
         {
             var comment = new ProjectComment(inputModel.Content, inputModel.IdProject, inputModel.IdUser);
 
-            _dbContext.ProjectComments.Add(comment);    
+            _dbContext.ProjectComments.Add(comment);
+            _dbContext.SaveChanges();
+
         }
 
         public void Delete(int id)
@@ -36,6 +39,8 @@ namespace DevFreela.Application.Services.Implementations
             var project = _dbContext.Projects.Single(p => p.Id == id);
 
             project.Cancel();
+            _dbContext.SaveChanges();
+
         }
 
         public void Finish(int id)
@@ -43,6 +48,8 @@ namespace DevFreela.Application.Services.Implementations
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
 
             project.Finish();
+            _dbContext.SaveChanges();
+
         }
 
         public List<ProjectViewModel> GetAll(string query)
@@ -79,6 +86,8 @@ namespace DevFreela.Application.Services.Implementations
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
 
             project.Start();
+            _dbContext.SaveChanges();
+
         }
 
         public void Update(UpdateProjectInputModel inputModel)
@@ -86,6 +95,7 @@ namespace DevFreela.Application.Services.Implementations
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == inputModel.Id);
 
             project.Update(inputModel.Title, inputModel.Description, inputModel.TotalCost);
+            _dbContext.SaveChanges();
 
         }
     }
