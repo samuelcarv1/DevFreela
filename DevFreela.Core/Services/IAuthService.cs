12f0683da20cbf0a8d138c83bba5d534c7a6d4cf
Samuel Carvalho <samuelcarvalho2000@outlook.com>
2025-09-03 23:09:07 -0300

    Feat: criando um Jwt

---
+++ DevFreela.Core/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Core.Services
+{
+    public interface IAuthService
+    {
+        string GenerateJwtToken(string email, string role);
+    }
+}
